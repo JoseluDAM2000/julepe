e54f0d9ef259b588793dd41896793538a5e1d50a
joselu <joseludam2000@gmail.com>
2017-02-02 11:04:06 +0100

    Añadido metodo cartaQueVaGanandoLaBaza a la clase Baza

--- Baza.java
+++ Baza.java
@@ -10,13 +10,15 @@ public class Baza
 {
     // Atributo que almacena la carta que es baza.
     private ArrayList<Carta> baza;
-
+    // Atributo que almacena el palo que pinta.
+    private int paloQuePinta;
     /**
      * Constructor for objects of class Baza
      */
     public Baza(int numeroJugadores, int paloQuePinta)
     {
         baza = new ArrayList<Carta>();
+        this.paloQuePinta = paloQuePinta;
     }
     
     /**
@@ -38,5 +40,23 @@ public class Baza
             palo = baza.get(0).getPalo();
         }
         return palo;
+    }
+    
+    /**
+     * 
+     */
+    public Carta cartaQueVaGanandoLaBaza()
+    {
+        Carta cartaQueGana = null;
+        if(baza.size() != 0){
+            cartaQueGana = baza.get(0);
+            for(int i=1; i < baza.size(); i++){
+                Carta cartaAspirante = baza.get(i);
+                if(cartaAspirante.ganaA(cartaQueGana, paloQuePinta)){
+                    cartaQueGana = cartaAspirante;
+                }
+            }
+        }
+        return cartaQueGana;
     }
 }
