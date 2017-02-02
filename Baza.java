59d10956968d1f785e68263027eda84a9739ce85
joselu <joseludam2000@gmail.com>
2017-02-02 10:49:40 +0100

    Añadido metodo addCarta a la clase Baza

--- Baza.java
+++ Baza.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 /**
  * Write a description of class Baza here.
@@ -7,13 +8,22 @@
  */
 public class Baza
 {
-    
+    // Atributo que almacena la carta que es baza.
+    private ArrayList<Carta> baza;
 
     /**
      * Constructor for objects of class Baza
      */
     public Baza(int numeroJugadores, int paloQuePinta)
     {
-        
+        baza = new ArrayList<Carta>();
+    }
+    
+    /**
+     * Metodo que recibe el nombre del jugador y una carta la cual almacena.
+     */
+    public void addCarta(Carta cartaTirada, String nombreJugador)
+    {
+        baza.add(cartaTirada);
     }
 }
