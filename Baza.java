d2eb6ba375fa92a09e04df4e51224f27a43b2c8a
joselu <joseludam2000@gmail.com>
2017-02-02 10:37:46 +0100

    Creada clase Baza

---
+++ Baza.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Baza here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Baza
+{
+    
+
+    /**
+     * Constructor for objects of class Baza
+     */
+    public Baza(int numeroJugadores, int paloQuePinta)
+    {
+        
+    }
+}
