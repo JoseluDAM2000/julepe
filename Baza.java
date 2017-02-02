4c60e878ccc8310b98b08e1db65175d29c344217
joselu <joseludam2000@gmail.com>
2017-02-02 11:14:20 +0100

    Añadido metodo nombreJugadorQueVaGanandoLaBaza a la clase Baza

--- Baza.java
+++ Baza.java
@@ -9,7 +9,9 @@ import java.util.ArrayList;
 public class Baza
 {
     // Atributo que almacena la carta que es baza.
-    private ArrayList<Carta> baza;
+    private Carta[] baza;
+    // Atributo que almacena en orden los jugadores que han tirado cartas.
+    private String[] jugadores;
     // Atributo que almacena el palo que pinta.
     private int paloQuePinta;
     /**
@@ -17,7 +19,8 @@ public class Baza
      */
     public Baza(int numeroJugadores, int paloQuePinta)
     {
-        baza = new ArrayList<Carta>();
+        baza = new Carta[numeroJugadores];
+        jugadores = new String[numeroJugadores];
         this.paloQuePinta = paloQuePinta;
     }
     
@@ -26,7 +29,8 @@ public class Baza
      */
     public void addCarta(Carta cartaTirada, String nombreJugador)
     {
-        baza.add(cartaTirada);
+        baza[baza.length] = cartaTirada;
+        jugadores[baza.length] = nombreJugador;
     }
     
     /**
@@ -36,27 +40,45 @@ public class Baza
     public int getPaloPrimeraCartaDeLaBaza()
     {
         int palo = -1;
-        if(baza.size() != 0){
-            palo = baza.get(0).getPalo();
+        if(baza.length != 0){
+            palo = baza[0].getPalo();
         }
         return palo;
     }
     
     /**
-     * 
+     * Devuelve la carta que va ganando la baza o null si no hay cartas aun.
      */
     public Carta cartaQueVaGanandoLaBaza()
     {
         Carta cartaQueGana = null;
-        if(baza.size() != 0){
-            cartaQueGana = baza.get(0);
-            for(int i=1; i < baza.size(); i++){
-                Carta cartaAspirante = baza.get(i);
+        if(baza.length != 0){
+            cartaQueGana = baza[0];
+            for(int i=1; i < baza.length; i++){
+                Carta cartaAspirante = baza[i];
                 if(cartaAspirante.ganaA(cartaQueGana, paloQuePinta)){
                     cartaQueGana = cartaAspirante;
                 }
             }
         }
         return cartaQueGana;
+    }
+    
+    /**
+     * Devuelve el nombre del jugador que va ganando la baza en el momento actual.
+     */
+    public String nombreJugadorQueVaGanandoLaBaza()
+    {
+        String nombreJugador = "";
+        int index = 0;
+        boolean buscando = true;
+        while(index < baza.length && buscando){
+            if(baza[index].equals(cartaQueVaGanandoLaBaza())){
+                nombreJugador = jugadores[index];
+                buscando = false;
+            }
+            index++;
+        }
+        return nombreJugador;
     }
 }
